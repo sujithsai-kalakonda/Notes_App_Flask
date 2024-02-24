c8e04503125692dd3f9232dc1f26f52ecf9bfefe
sujithkalakonda-techolution <sujith.kalakonda@techolution.com>
2024-02-24 19:19:41 +0530

    Added style for notes placement and deleting notes.

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
         else:
             flash("Email does not exist", category="error")
 
-    return render_template("login.html", boolean=True)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
@@ -66,4 +66,4 @@ def sign_up():
             flash("Successfully account created", category="success")
             return redirect(url_for("views.home"))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
